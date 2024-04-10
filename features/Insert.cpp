983364946877467e7877285ba8732743663da0da
Rafael Guterres Jeffman <rafasgj@gmail.com>
2024-04-10 04:13:15 +0000

    Corrigidos os testes Insert e Remove.

--- features/Insert.cpp
+++ features/Insert.cpp
@@ -12,37 +12,41 @@ SCENARIO("Inserir valores num array.", "[Insert]") {
         for (int i=0; i < 5; i++) {
             GenericArray_append(&array, &i);
         }
-        int original_size = GenericArray_size(&array);
 
         WHEN ("Inserir um valor após o último elemento") {
+	    int original_size = GenericArray_size(&array);
             int new_value = 10;
             int index = GenericArray_size(&array);
             int prev;
             GenericArray_get(&array, index-1, &prev);
             GenericArray_insert(&array, index, &new_value);
 
+            THEN("O tamanho do array foi alterado em um elemento.") {
+                REQUIRE(original_size + 1 == GenericArray_size(&array));
+            }
             THEN("O último valor é o inserido.") {
                 int value = 0;
                 GenericArray_get(&array, index, &value);
                 REQUIRE(value == new_value);
             }
-            THEN("O último valor anterior, ainda é válido.") {
+            THEN("O valor anterior ao último, é o mesmo do último antes da alteração.") {
                 int value = 0;
                 GenericArray_get(&array, index-1, &value);
 
                 REQUIRE(prev == value);
             }
-            THEN("O tamanho do array foi alterado em um elemento.") {
-                REQUIRE(original_size + 1 == GenericArray_size(&array));
-            }
         }
         
         WHEN ("Inserir um valor antes do primeiro elemento") {
+            int original_size = GenericArray_size(&array);
             int new_value = 100;
             int next;
             GenericArray_get(&array, 0, &next);
             GenericArray_insert(&array, 0, &new_value);
 
+            THEN("O tamanho do array foi alterado em um elemento.") {
+                REQUIRE(original_size + 1 == GenericArray_size(&array));
+            }
             THEN("O primeiro valor é o inserido.") {
                 int value = 0;
                 GenericArray_get(&array, 0, &value);
@@ -55,12 +59,10 @@ SCENARIO("Inserir valores num array.", "[Insert]") {
 
                 REQUIRE(next == value);
             }
-            THEN("O tamanho do array foi alterado em um elemento.") {
-                REQUIRE(original_size + 2 == GenericArray_size(&array));
-            }
         }
 
         WHEN ("Inserir um valor na antepenúltima posição.") {
+            int original_size = GenericArray_size(&array);
             int new_value = 100;
             int index = GenericArray_size(&array)-3;
             int prev, next;
@@ -68,6 +70,9 @@ SCENARIO("Inserir valores num array.", "[Insert]") {
             GenericArray_get(&array, index, &next);
             GenericArray_insert(&array, index, &new_value);
 
+            THEN("O tamanho do array foi alterado em um elemento.") {
+                REQUIRE(original_size + 1 == GenericArray_size(&array));
+            }
             THEN("Os valores anterior e próximo ainda são válidos.") {
                 int value = 0;
                 GenericArray_get(&array, index-1, &value);
@@ -79,9 +84,6 @@ SCENARIO("Inserir valores num array.", "[Insert]") {
                 int value = 0;
                 GenericArray_get(&array, index, &value);
                 REQUIRE(new_value == value);
-            }
-            THEN("O tamanho do array foi alterado em um elemento.") {
-                REQUIRE(original_size + 3 == GenericArray_size(&array));
             }
         }
 
